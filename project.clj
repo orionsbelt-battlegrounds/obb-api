d218f11c90c61cdb4c8b910025faa11d7b64bd61
Pedro Santos <donbonifacio@gmail.com>
2014-12-10 09:47:49 +0000

    Add newrelic

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :java-agents [[com.newrelic.agent.java/newrelic-agent "3.12.0"]]
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.8.0"]
                  [clj-jwt "0.0.11"]
