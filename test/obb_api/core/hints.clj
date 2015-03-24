1397e608681f81f8e37754ead0e553b191c6716e
Pedro Santos <donbonifacio@gmail.com>
2015-03-24 21:51:35 +0000

    Translate hints

--- test/obb_api/core/hints.clj
+++ test/obb_api/core/hints.clj
@@ -3,16 +3,21 @@
   (:require [obb-rules.game :as game]
             [obb-rules.element :as element]
             [obb-rules.simplifier :as simplify]
+            [obb-rules.translator :as translator]
             [obb-rules.actions.move :as move]
             [obb-rules.board :as board]))
 
+(defn- all-possible-moves
+  "Gets all possible moves, with the coordinate translated"
+  [board viewer element]
+  (->> (move/find-all-possible-destinations-with-cost board element)
+       (reduce (fn [col [coord cost]] (assoc col (translator/coordinate viewer coord) cost)) {})))
+
 (defn- hints-for-element
   "Gathers the hints for a specific element"
-  [board element]
-  ;{:pre [(element/assert-element element)]}
-  {:coord (element/element-coordinate element)
-   :goto (move/find-all-possible-destinations-with-cost board element)
-   })
+  [board viewer element]
+  {:coord (translator/coordinate viewer (element/element-coordinate element))
+   :goto (all-possible-moves board viewer element)})
 
 (defn for-game
   "Returns to the given hash hints to use to move the elements"
@@ -20,7 +25,7 @@
   (when (and viewer (simplify/name= viewer (get-in game [:board :state])))
     (let [board (:board game)
           elements (board/board-elements board viewer)
-          hints-for-element (partial hints-for-element board)
+          hints-for-element (partial hints-for-element board viewer)
           hints (map hints-for-element elements)]
       hints)))
 
